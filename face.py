702b25f91d9c3975b8be8379d5db3c0b21f65682
Chetan Allapur <54956965+ChetanAllapur@users.noreply.github.com>
2020-07-24 01:21:41 +0530

    Update face.py

--- face.py
+++ face.py
@@ -34,27 +34,17 @@ except Exception as e:
     print(e)
 #----------------------------------------------------------------------------------------
 #For dynamically loading the image list from snowflake database
-sql = "select image from users"
-cursor = conn.cursor()
-cursor.execute(sql)
+cur = conn.cursor()
 
-users_image = []
-users_face_encoding = []
-known_face_encodings = []
-known_face_names = []
+cur.execute("select * from users")
+rows = cur.fetchall()
 
-for a in cursor:
-    z = face_recognition.load_image_file(a[0])
-    users_image.append(z)
-    users_face_encoding.append(face_recognition.face_encodings(z)[0])
 
-for s in users_face_encoding:
-    known_face_encodings.append(s)
+known_face_names=[]
+known_face_encodings =[]
+for c in rows:
+    known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(c[1]))[0])
 
-sql = "select name from users"
-cursor = conn.cursor()
-cursor.execute(sql)
-for c in cursor:
     known_face_names.append(c[0])
 
 #----------------------------------------------------------------------------------------
@@ -66,7 +56,7 @@ while True:
 
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
+    #----------------------------------------------------------------------------------------
     # loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
@@ -77,9 +67,9 @@ while True:
             first_match_index = matches.index(True)
             name = known_face_names[first_match_index]
 
-        cv2.rectangle(frame, (left , bottom - 120 ), (right , bottom ), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left , bottom - 120 ), (right , bottom ), (0, 255, 255), 2)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom ), font, 0.7, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom ), font, 0.7, (0, 0, 255), 1)
         if (name != "Unknown Person"):
             print(name, "was here")
     cv2.imshow('Video', frame)
@@ -87,4 +77,5 @@ while True:
     # Q to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 #----------------------------------------------------------------------------------------
